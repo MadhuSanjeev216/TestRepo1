aed462227625d076f1730b8e083c8934ea633e74
sanjeev <madhusanjeev216@gmail.com>
2023-10-29 23:55:53 -0400

    four programs completed

---
+++ secondProgram.js
@@ -0,0 +1,16 @@
+let a = 1, b = 2.71888, c = 'H', d = "Hello World", e = true;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log("              ")
+
+var z = 1, y = 2.71888,  x= 'H', p = "Hello World", q = false;
+
+console.log(typeof(p));
+console.log(typeof(q));
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
