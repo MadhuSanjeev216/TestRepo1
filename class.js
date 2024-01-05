0fa6f9a6bd9340dc655514b1afc0e3766d429bc9
sanjeev <madhusanjeev216@gmail.com>
2024-01-05 14:46:56 -0500

    changes to class program

--- class.js
+++ class.js
@@ -1,13 +1,14 @@
-
+let age
 class Car {
     constructor(nameOfCar, yearOfManufactured){
         this.nameOfCar = nameOfCar;
         this.yearOfManufactured = yearOfManufactured;
     }
     age(currentYear) {
-        let age = currentYear  - this.yearOfManufactured;
+        age = currentYear  - this.yearOfManufactured;
         console.log(`Current Age of car is ${age}`);
     }
 }
 const objOfCar = new Car('Ford', 2012);
 objOfCar.age(2022)
+console.log(`Name of the car is ${objOfCar.nameOfCar} and it's manufactured in the year ${objOfCar.yearOfManufactured}, now it's age is about ${age} years`)
